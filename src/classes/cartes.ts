6cb206b0a8e3eeab4bdd8f274622983c2380ec57
Michal <mwargan@gmail.com>
2022-07-28 10:47:04 +0200

    Switched to using general cartes class for requests

---
+++ src/classes/cartes.ts
@@ -0,0 +1,136 @@
+
+class cartes {
+
+    #api_url: string = "https://cartes.io/api/";
+    #map_uuid: string | null;
+    #api_key: string | null;
+    #token: string | null;
+    #headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    };
+    #params: Record<string, any>;
+    #request_url = this.#api_url;
+
+    // Constructor with map_uuid=None, api_key=None, token=None
+    // Constructor with map_uuid=None, api_key=None, token=None
+    constructor(map_uuid = null as string | null, api_key = null as string | null, token = null as string | null) {
+        this.#map_uuid = map_uuid;
+        this.#api_key = api_key;
+        this.#token = token;
+
+        this.#params = {
+            token: this.#token,
+            api_key: this.#api_key,
+        }
+
+        if (map_uuid) {
+            this.#request_url = this.#api_url + "maps/" + map_uuid;
+        }
+    }
+
+    // Private method to get headers
+    private getHeaders(): Headers {
+        const headers = new Headers();
+        headers.append("Accept", "application/json");
+        headers.append("Content-Type", "application/json");
+        headers.append("Authorization", "Bearer " + this.#api_key);
+        return headers;
+    }
+
+    private attachParamsToUrl() {
+        // Attach any params to this.#request_url
+        if (this.#params) {
+            let params = "";
+            for (let key in this.#params) {
+                if (this.#params[key]) {
+                    params += "&" + key + "=" + this.#params[key];
+                }
+            }
+            this.#request_url += "?" + params.substring(1);
+        }
+    }
+
+    // Private method handleRequest to make the request using fetch
+    private handleRequest(method = "GET", body = null as any): Promise<any> {
+
+        this.attachParamsToUrl();
+
+        const data = {
+            method: method.toUpperCase(),
+            headers: this.getHeaders(),
+        } as RequestInit;
+
+        // If the method is not GET, we need to add the body
+        if (data.method !== "GET") {
+            data.body = JSON.stringify(body);
+        }
+
+        return fetch(this.#request_url, data)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                // Switch the response codes to determine the message
+                switch (response.status) {
+                    case 400:
+                        return response.json().then((data) => {
+                            throw new Error(data.message);
+                        });
+                    case 401:
+                        throw new Error("You are not logged in.");
+                    case 403:
+                        throw new Error("You are not authorized to add markers to this map.");
+                    case 404:
+                        throw new Error("Map not found.");
+                    case 500:
+                        throw new Error("Internal server error.");
+                    default:
+                        throw new Error("Unknown error.");
+                }
+            })
+            .then((data) => {
+                console.log("Got");
+                return data;
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+
+    public maps(uuid = null as string | number | null, token = null as string | null): cartes {
+        if (uuid) {
+            this.#request_url = this.#api_url + "maps/" + uuid;
+        } else {
+            this.#request_url = this.#api_url + "maps";
+        }
+
+        if (token) {
+            this.#params.token = token;
+        }
+
+        return this;
+    }
+
+    // Public method "get"
+    public get(): Promise<any> {
+        return this.handleRequest("GET");
+    }
+
+    // Public method "create"
+    public create(data: any): Promise<any> {
+        return this.handleRequest("POST", data);
+    }
+
+    // Public method "update"
+    public update(data: any): Promise<any> {
+        return this.handleRequest("PUT", data);
+    }
+
+    // Public method "delete"
+    public delete(): Promise<any> {
+        return this.handleRequest("DELETE");
+    }
+}
+
+export default new cartes();
--- src/composables/map.ts
+++ src/composables/map.ts
@@ -2,6 +2,7 @@ import userDevice from "@/classes/userDevice";
 import { Map, MapForm } from "@/types/map";
 import { computed } from "@vue/reactivity";
 import { PropType, defineEmits, getCurrentInstance, ref, reactive } from "vue";
+import cartes from "@/classes/cartes";
 
 const maps = ref<Map[]>([]);
 
@@ -47,41 +48,16 @@ export function useMap() {
         if (!userDevice.online) {
             return alert("You must be online to get all maps.");
         }
-        // Fetch the maps from the api https://cartes.io/api/maps
-        fetch("https://cartes.io/api/maps", {
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                maps.value = data.data;
-                return data.data;
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
+        const data = await cartes.maps().get();
+        maps.value = data.data;
     }
 
-    const getMap = (mapId: string | number) => {
+    const getMap = async (mapId: string | number) => {
         if (!userDevice.online) {
             return alert("You must be online to get a map.");
         }
-        // Fetch the markers from the api https://cartes.io/api/maps/3bdc0bdc-8a77-40e3-8c34-c70466443980
-        fetch("https://cartes.io/api/maps/" + mapId, {
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                Object.assign(map, data) // equivalent to reassign
-            }
-            )
-            .catch((error) => {
-                console.error("Error:", error);
-            }
-            );
+        const data = await cartes.maps(mapId).get();
+        Object.assign(map, data) // equivalent to reassign
     }
 
     const hasErrors = computed(() => {
@@ -95,75 +71,22 @@ export function useMap() {
         return !hasErrors.value;
     };
 
-    const addMap = (data = null as MapForm | null, redirect = false) => {
-        if (data && !validateMapForm(data)) {
+    const addMap = async (formData = null as MapForm | null, redirect = false) => {
+        if (formData && !validateMapForm(formData)) {
             return;
         };
         if (!userDevice.online) {
             return alert("You must be online to add a map.");
         }
         isLoading.value = true;
-        fetch("https://cartes.io/api/maps", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                // Switch the response codes to determine the message
-                switch (response.status) {
-                    case 400:
-                        return response.json().then((data) => {
-                            throw new Error(data.message);
-                        });
-                    case 401:
-                        throw new Error("You are not logged in.");
-                    case 403:
-                        throw new Error("You are not authorized to add maps to this map.");
-                    case 404:
-                        throw new Error("The map does not exist.");
-                    case 422:
-                        return response.json().then((data) => {
-                            console.error("Malformed data: ", data);
-                            // Fill the formErrors
-                            Object.keys(data.errors).forEach((key) => {
-                                // @ts-ignore
-                                formErrors[key] = data.errors[key][0];
-                            });
+        const data = await cartes.maps().create(formData);
+        console.log("New map: ", data);
+        localStorage["map_" + data.uuid] = data.token;
+        if (redirect) {
+            window.location.href = "?mapId=" + data.uuid;
+        }
+        emit("addedMap", data);
 
-                            throw new Error(data.message);
-                        });
-                    case 429:
-                        throw new Error("You are creating maps too quickly. Please wait a moment and try again.");
-                    default:
-                        throw new Error("The server encountered an error.");
-                }
-            })
-            .then((data: Map) => {
-                if (data.uuid) {
-                    // if (data.category.icon && !data.category.icon.startsWith("https")) {
-                    //     data.category.icon = "/map.svg";
-                    // }
-                    console.log("New map: ", data);
-                    localStorage["map_" + data.uuid] = data.token;
-                    if (redirect) {
-                        window.location.href = "?mapId=" + data.uuid;
-                    }
-                    emit("addedMap", data);
-                }
-            })
-            .catch((error) => {
-                alert(error.message || "An unknown request error occurred.");
-                console.error("Error:", error);
-            })
-            .finally(() => {
-                isLoading.value = false;
-            });
     };
 
     const canDeleteMap = (map: Map) => {
@@ -178,101 +101,34 @@ export function useMap() {
         return map.users_can_create_markers === 'yes' || map.token || localStorage.getItem("map_" + map.uuid);
     }
 
-    const updateMap = (map: Map, data: MapForm) => {
+    const updateMap = async (map: Map, formData: MapForm) => {
         if (!canUpdateMap(map)) {
             return;
         }
         if (!userDevice.online) {
             return alert("You must be online to update a map.");
         }
-        // Add token: map.token || localStorage.getItem("map_" + map.uuid), to the data
-        data.token = map.token || localStorage.getItem("map_" + map.uuid);
 
         isLoading.value = true;
-        fetch("https://cartes.io/api/maps/" + map.uuid, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                // Switch the response codes to determine the message
-                switch (response.status) {
-                    case 400:
-                        return response.json().then((data) => {
-                            throw new Error(data.message);
-                        });
-                    case 401:
-                        throw new Error("You are not logged in.");
-                    case 403:
-                        throw new Error("You are not authorized to update this map.");
-                    case 404:
-                        throw new Error("The map does not exist.");
-                    case 422:
-                        return response.json().then((data) => {
-                            console.error("Malformed data: ", data);
-                            // Fill the formErrors
-                            Object.keys(data.errors).forEach((key) => {
-                                // @ts-ignore
-                                formErrors[key] = data.errors[key][0];
-                            });
-
-                            throw new Error(data.message);
-                        });
-                    case 429:
-                        throw new Error("You are updating maps too quickly. Please wait a moment and try again.");
-                    default:
-                        throw new Error("The server encountered an error.");
-                }
-            })
-            .then((data: Map) => {
-                if (data.uuid) {
-                    emit("updatedMap", data);
-                }
-            })
-            .catch((error) => {
-                alert(error.message || "An unknown request error occurred.");
-                console.error("Error:", error);
-            })
-            .finally(() => {
-                isLoading.value = false;
-            });
+        const data = await cartes.maps(map.uuid, map.token || localStorage.getItem("map_" + map.uuid)).update(formData);
+        if (data.uuid) {
+            emit("updatedMap", data);
+        }
+        isLoading.value = false;
     };
 
-    const deleteMap = (map: Map) => {
+    const deleteMap = async (map: Map) => {
         if (!userDevice.online) {
             return alert("You must be online to delete a map.");
         }
         // Check that the map exists and that it has a token field
         if (canDeleteMap(map)) {
             // Delete the map
-            fetch(`https://cartes.io/api/maps/${map.uuid}`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                },
-                body: JSON.stringify({
-                    token: map.token || localStorage.getItem("map_" + map.uuid),
-                }),
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        localStorage.removeItem("map_" + map.uuid);
-                        alert("Map deleted.");
-                        window.location.href = "/";
-                        emit('deletedMap', map);
-                    }
-                })
-                .catch((error) => {
-                    alert(error);
-                    console.error(error);
-                });
+            await cartes.maps(map.uuid, map.token || localStorage.getItem("map_" + map.uuid)).delete();
+            localStorage.removeItem("map_" + map.uuid);
+            alert("Map deleted.");
+            window.location.href = "/";
+            emit('deletedMap', map);
         } else {
             alert("This map could not be deleted.");
         }
