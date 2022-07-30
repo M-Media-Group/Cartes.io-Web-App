124845e3f89e10abe454502ccb8d7c5fabe30f08
Michal <mwargan@gmail.com>
2022-07-30 19:17:33 +0200

    Added marker layer group

--- src/components/maps/MapMarkers.vue
+++ src/components/maps/MapMarkers.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useMarker } from '@/composables/marker';
 import { Marker } from '@/types/marker';
-import { PropType } from 'vue';
+import { computed, PropType } from 'vue';
 import {
     LMap,
     LIcon,
@@ -11,12 +11,13 @@ import {
     LTooltip,
     LPopup,
     LLayerGroup,
+    LFeatureGroup,
     LControl,
 } from "@vue-leaflet/vue-leaflet";
 
-defineProps({
+const props = defineProps({
     mapId: {
-        type: String as PropType<string | number>,
+        type: String,
         required: true,
     },
     markers: {
@@ -27,49 +28,67 @@ defineProps({
 
 const { canDeleteMarker, deleteMarker, canCreateMarker } = useMarker();
 
+const groupedMarkersByCategory = computed(() => {
+    const markers = props.markers;
+    const groupedMarkers = {} as Record<number, Marker[]>;
+    markers.forEach(marker => {
+        if (!groupedMarkers[marker.category_id]) {
+            groupedMarkers[marker.category_id] = [];
+        }
+        groupedMarkers[marker.category_id].push(marker);
+    });
+    return groupedMarkers;
+});
+
 </script>
 
 <template>
-    <l-marker v-for="marker in markers"
-        :lat-lng="[marker.location.coordinates[1], marker.location.coordinates[0]]"
-        :key="marker.id + 'marker'">
-        <l-icon :icon-url="marker.category.icon"
-            :icon-size="[30, 30]"
-            :icon-anchor="[15, 25]" />
-        <l-popup>
-            <p style="min-width: 200px">
-                <b>{{ marker.category.name }}</b>
-            </p>
-            <p v-if="marker.description"
-                v-html="marker.description"></p>
-            <small v-if="marker.link"><a :href="marker.link"
-                    target="blank">{{
-                            marker.link.split("/")[2]
-                    }}</a>
-            </small>
-            <small>Last update:
-                <span :datetime="marker.updated_at">{{
-                        marker.updated_at
-                }}</span>.
-            </small>
-            <small v-if="marker.elevation">Elevation:
-                {{ marker.elevation }} meters
-            </small>
-            <!-- <small v-if="isMarkerExpired(marker.expires_at)" >Expired:
+    <l-layer-group v-for="(category, index) in groupedMarkersByCategory"
+        :key="index"
+        ref="features"
+        layer-type="overlay"
+        :name="category[0].category.name">
+        <l-marker v-for="marker in category"
+            :lat-lng="[marker.location.coordinates[1], marker.location.coordinates[0]]"
+            :key="marker.id + 'marker'">
+            <l-icon :icon-url="marker.category.icon"
+                :icon-size="[30, 30]"
+                :icon-anchor="[15, 25]" />
+            <l-popup>
+                <p style="min-width: 200px">
+                    <b>{{ marker.category.name }}</b>
+                </p>
+                <p v-if="marker.description"
+                    v-html="marker.description"></p>
+                <small v-if="marker.link"><a :href="marker.link"
+                        target="blank">{{
+                                marker.link.split("/")[2]
+                        }}</a>
+                </small>
+                <small>Last update:
+                    <span :datetime="marker.updated_at">{{
+                            marker.updated_at
+                    }}</span>.
+                </small>
+                <small v-if="marker.elevation">Elevation:
+                    {{ marker.elevation }} meters
+                </small>
+                <!-- <small v-if="isMarkerExpired(marker.expires_at)" >Expired:
             <span  :datetime="marker.expires_at">{{
                 marker.expires_at
             }}</span>.</small> -->
-            <!-- <details >
+                <!-- <details >
             <summary
               @click='searchLocation(marker.location.coordinates[0] + " " + marker.location.coordinates[1], false)'>
               Click to see address</summary>
             <p>{{ searchResults }}</p>
           </details> -->
-            <a v-if="canDeleteMarker(marker)"
-                @click="deleteMarker(mapId, marker)">Delete</a>
-            <!--
+                <a v-if="canDeleteMarker(marker)"
+                    @click="deleteMarker(mapId, marker)">Delete</a>
+                <!--
           <a  v-if="canMarkAsSpamPost(marker)" @click="markAsSpam(marker.id)"
             :disabled="submit_data.loading">Report as spam</a> -->
-        </l-popup>
-    </l-marker>
+            </l-popup>
+        </l-marker>
+    </l-layer-group>
 </template>
