08d3a6cf858e4daf572a0e480790e61dc6e9acb9
Michal <mwargan@gmail.com>
2022-08-01 23:13:39 +0200

    Added developer info

--- src/App.vue
+++ src/App.vue
@@ -122,6 +122,45 @@ const share = async () => {
                 :map="Maps.map" />
             </details>
 
+            <!-- Developer -->
+            <details>
+              <summary>Developer info</summary>
+              <p>
+                Use standard API requests to get data from this map. No
+                authentication required for public and unlisted maps.
+                <a href="https://github.com/M-Media-Group/Cartes.io/wiki/API"
+                  rel="noopener"
+                  target="_BLANK">Read the docs</a>.
+              </p>
+              <ul>
+                <li>Use the API to get the map and markers
+                  <!-- Code block showing API call -->
+                  <pre><code>GET https://cartes.io/api/maps/{{ Maps.map.uuid }}
+GET https://cartes.io/api/maps/{{ Maps.map.uuid }}/markers</code></pre>
+                </li>
+                <li>Embed as iFrame
+                  <!-- Code block showing API call -->
+                  <pre><code>&lt;iframe src="https://cartes.io/embeds/maps/{{ Maps.map.uuid }}?type=map"
+  width="100%"
+  height="400"
+  frameborder="0"&gt;&lt;/iframe&gt;</code></pre>
+                </li>
+                <li>Shortcode using our WordPress plugin
+                  <!-- Code block showing API call -->
+                  <pre><code>[cartes_map uuid="{{ Maps.map.uuid }}"]</code></pre>
+                </li>
+                <li>Python using our Python package
+                  <!-- Code block showing API call -->
+                  <pre><code>cartes.Maps('{{ Maps.map.uuid }}').get()</code></pre>
+                </li>
+                <li>JS using our NPM package
+                  <!-- Code block showing API call -->
+                  <pre><code>cartes.maps('{{ Maps.map.uuid }}').get()</code></pre>
+                </li>
+              </ul>
+              <p>When using the API or embedding the map, you must attribute this website on your front-end.</p>
+            </details>
+
           </div>
         </section>
 
