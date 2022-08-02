eb877c08e33a688623c557480721771edc775291
Michal <mwargan@gmail.com>
2022-08-02 14:25:27 +0200

    Added more types

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -3,3 +3,7 @@ declare module '@vue-leaflet/vue-leaflet';
 declare module '@vue-leaflet/vue-leaflet/src/functions/layer';
 
 declare module '@vue-leaflet/vue-leaflet/src/utils';
+
+declare module 'leaflet/dist/leaflet-src.esm';
+
+declare module 'leaflet.markercluster/dist/leaflet.markercluster-src.js';
