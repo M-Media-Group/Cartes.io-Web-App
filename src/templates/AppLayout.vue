2d971c36ce090e20f5286d1579848653a09911be
Michal <mwargan@gmail.com>
2022-08-05 19:53:28 +0200

    Added page transitions

--- src/templates/AppLayout.vue
+++ src/templates/AppLayout.vue
@@ -7,15 +7,39 @@ import AppFooter from "@/components/AppFooter.vue";
     <slot name="nav">
         <NavBar />
     </slot>
-    <header v-if="$slots.header">
-        <slot name="header"></slot>
-    </header>
-    <main class="container">
-        <slot />
+    <Transition appear>
+        <header key="header"
+            v-if="$slots.header">
+
+            <slot name="header"></slot>
+
+        </header>
+    </Transition>
+    <main key="main"
+        class="container">
+        <Transition appear>
+
+            <slot />
+        </Transition>
     </main>
-    <footer class="container">
+
+
+    <footer key="footer"
+        class="container">
         <slot name="footer">
             <AppFooter />
         </slot>
     </footer>
 </template>
+<style>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.15s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
