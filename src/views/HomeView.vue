627d01ffa2c6aeff9077be0bb8a425589114ddad
Michal <mwargan@gmail.com>
2022-08-08 14:18:38 +0200

    Updated homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import { Map } from '@/types/map';
 import { useMarker } from '@/composables/marker';
 import { useUser } from '@/composables/user';
 import MapArticle from '@/components/MapArticle.vue';
+import CreateMapButton from '@/components/CreateMapButton.vue';
 
 const Maps = useMap();
 Maps.getAllMaps().then(() => {
@@ -33,6 +34,7 @@ getUsers();
 const privateMaps = ref([]) as Ref<Map[]>;
 
 var ids = [] as string[];
+
 Object.keys(localStorage).forEach(function (key) {
     if (key.includes("map_")) {
         ids.push(key.replace("map_", ""));
@@ -80,28 +82,21 @@ watch(() => user.value?.id, () => {
         <template #header>
             <div class="header">
                 <div class="container">
-                    <div class="headings">
-                        <h1>Maps for everyone and everything</h1>
-                        <p>Create maps, add markers, and share anywhere without even having to sign up.</p>
-                    </div>
-
+                    <BaseHeading title="Maps for everyone and everything"
+                        subtitle="Create maps, add markers, and share anywhere without even having to sign up." />
                     <div class="grid">
 
-                        <button @click="Maps.addMap(null, true);">
-                            Create a new map
-                        </button>
+                        <create-map-button text="Create a new map" />
 
                         <div>
-                            <router-link class="secondary"
-                                role="button"
+                            <BaseButton class="secondary"
                                 v-if="!user"
                                 to="/register">Sign up, if you want
-                            </router-link>
-                            <router-link class="secondary"
-                                role="button"
+                            </BaseButton>
+                            <BaseButton class="secondary"
                                 v-else
                                 to="/me">My account
-                            </router-link>
+                            </BaseButton>
                         </div>
                     </div>
                 </div>
@@ -109,64 +104,55 @@ watch(() => user.value?.id, () => {
         </template>
         <div class="grid column-4-1-grid">
             <div>
-                <section>
-                    <div class="headings">
-                        <h2>Your maps</h2>
-                        <p>These are the maps that you've created on the site.</p>
-                    </div>
+                <BaseSection title="Your maps"
+                    subtitle="These are the maps that you've created on the site.">
 
                     <MapArticle v-for="map in privateMaps"
                         :key="map.uuid"
                         :map="map" />
 
                     <article v-if="privateMaps.length === 0">
-                        <div class="headings">
-                            <h3>You have no maps yet.</h3>
-                            <p>Create your first map or browse the public ones.</p>
-                        </div>
-                        <button @click="Maps.addMap(null, true);">
-                            Create a new map
-                        </button>
+                        <BaseHeading as="h3"
+                            title="You have no maps yet."
+                            subtitle="Create your first map or browse the public ones." />
+                        <create-map-button text="Create a new map" />
                     </article>
-                </section>
+                </BaseSection>
+                <BaseSection title="Public maps"
+                    subtitle="These maps are made by the community and shared with everyone.">
 
-                <section>
-                    <div class="headings">
-                        <h2>Public maps</h2>
-                        <p>These maps are made by the community and shared with everyone.</p>
-                    </div>
                     <div>
+
                         <MapArticle v-for="(map, i) in Maps.maps.value"
+                            @click="$router.push('/maps/' + map.uuid)"
                             :key="map.uuid"
                             :map="map"
-                            :showMap="i < 2 ? true : false" />
+                            :showDescription="true"
+                            :showMap="true"
+                            :showFooter="false" />
+
+
                     </div>
                     <div>Showing {{ Maps.maps.value.length }} out of {{ Maps.totalMaps.value }} public maps and
                         many more
                         private ones
                     </div>
-                </section>
+                </BaseSection>
 
-                <section>
-                    <div class="headings">
-                        <h2>Public profiles</h2>
-                        <p>These profiles are public on Cartes.io</p>
-                    </div>
-                    <div>
-                        <article v-for="user in users">
-                            <h3>{{ user.username }}</h3>
-                            <p v-if="user.description">{{ user.description }}</p>
-                            <router-link :to="`/users/${user.username}`"
-                                custom
-                                v-slot="{ navigate }">
-                                <button @click="navigate">View profile</button>
-                            </router-link>
-                            <footer v-if="user.public_maps_count">
-                                {{ user.public_maps_count }} public maps
-                            </footer>
-                        </article>
-                    </div>
-                </section>
+                <BaseSection title="Public profiles"
+                    subtitle="These profiles are public on Cartes.io">
+
+                    <article v-for="user in users">
+                        <BaseHeading as="h3"
+                            :title='user.username' />
+                        <p v-if="user.description">{{ user.description }}</p>
+                        <BaseButton :to="`/users/${user.username}`">View profile</BaseButton>
+                        <footer v-if="user.public_maps_count">
+                            {{ user.public_maps_count }} public maps
+                        </footer>
+                    </article>
+
+                </BaseSection>
             </div>
             <aside>
                 <h4>Cartes.io.</h4>
@@ -176,6 +162,12 @@ watch(() => user.value?.id, () => {
                     <router-link to="/register">Sign up</router-link> to Cartes.io to get more info, make
                     maps private, and get updates as the project grows.
                 </p>
+
+                <iframe src="https://github.com/sponsors/M-Media-Group/button"
+                    title="Sponsor M-Media-Group"
+                    height="35"
+                    width="116"
+                    style="border: 0;"></iframe>
             </aside>
         </div>
     </AppLayout>
@@ -201,6 +193,92 @@ watch(() => user.value?.id, () => {
 
     .column-4-1-grid {
         grid-template-columns: 4fr 1fr;
+    }
+}
+
+.scrollableGrid {
+    grid-template-rows: 1fr 1fr;
+    grid-auto-flow: column;
+    overflow-y: scroll;
+    width: 100%;
+    grid-template-columns: unset;
+
+    width: 99vw;
+    margin-left: -4rem;
+    align-items: flex-start;
+
+    grid-auto-columns: 1fr;
+}
+
+.scrollableGrid>* {
+    min-width: unset;
+    align-items: flex-start;
+}
+
+
+
+
+.horizontal-media-scroller {
+    --size: 150px;
+    display: grid;
+    grid-auto-columns: minmax(60vw, 280px);
+    grid-auto-flow: column;
+    grid-auto-flow: column;
+    gap: calc(var(--grid-spacing-horizontal) / 2);
+    margin: 0;
+    padding-inline-end: var(--grid-spacing-horizontal);
+    padding-block-start: calc(var(--grid-spacing-horizontal) / 2);
+    padding-block-end: calc(var(--grid-spacing-horizontal) / 2);
+    overflow-x: auto;
+    overscroll-behavior-inline: contain;
+    scroll-snap-type: inline mandatory;
+    scroll-padding-left: var(--grid-spacing-horizontal);
+    scroll-padding-right: var(--grid-spacing-horizontal);
+    scroll-padding-inline: var(--grid-spacing-horizontal);
+}
+
+@media (prefers-reduced-motion: no-preference) {
+    .horizontal-media-scroller {
+        scroll-behavior: smooth;
+    }
+}
+
+.horizontal-media-scroller>li {
+    display: inline-block;
+    /*  container padding fix  */
+}
+
+.horizontal-media-scroller>li:last-of-type figure {
+    position: relative;
+}
+
+.horizontal-media-scroller>li:last-of-type figure::after {
+    content: "";
+    position: absolute;
+    inline-size: var(--grid-spacing-horizontal);
+    block-size: 100%;
+    inset-block-start: 0;
+    inset-inline-end: calc(var(--grid-spacing-horizontal) * -1);
+}
+
+.horizontal-media-scroller figure {
+    scroll-snap-align: start;
+}
+
+.horizontal-media-scroller a {
+    display: inline-block;
+    text-decoration: none;
+    color: inherit;
+    outline-offset: 12px;
+}
+
+.horizontal-media-scroller a:focus {
+    outline-offset: 7px;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+    .horizontal-media-scroller a {
+        transition: outline-offset 0.25s ease;
     }
 }
 </style>
