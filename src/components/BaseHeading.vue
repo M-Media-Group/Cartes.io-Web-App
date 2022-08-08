084936913cca089b3dde04e42093818370c29d0d
Michal <mwargan@gmail.com>
2022-08-08 10:50:25 +0200

    Added base heading component

---
+++ src/components/BaseHeading.vue
@@ -0,0 +1,26 @@
+
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    subtitle: {
+        type: String,
+        required: false,
+    },
+    as: {
+        type: String,
+        default: "h1"
+    },
+})
+</script>
+<template>
+    <div class="headings"
+        v-if="subtitle">
+        <component :is="as">{{ title }}</component>
+        <p>{{ subtitle }}</p>
+    </div>
+    <component v-else
+        :is="as">{{ title }}</component>
+</template>
