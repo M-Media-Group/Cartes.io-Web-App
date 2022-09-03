961234e639e4d702d24d2c80ff52619dace81b7d
Michal <mwargan@gmail.com>
2022-09-03 10:51:54 +0200

    Removed sync

--- src/components/maps/NewMapComponent.vue
+++ src/components/maps/NewMapComponent.vue
@@ -149,7 +149,7 @@ const setReady = async () => {
       v-model:max-bounds="maxBounds"
       :maxZoom="19"
       v-model:zoom="zoom"
-      v-model:center.sync="center"
+      v-model:center="center"
       @contextmenu="openAddMarkerPopup($event)"
       @ready="setReady"
       :options="{ attributionControl: false }"
