8e5c365b248032eea31a9c61e71cf06eed6abe4d
Michal <mwargan@gmail.com>
2022-09-10 14:17:28 +0200

    Added manifest and Netlify config for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,26 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      includeAssets: ['/android-chrome-192x192.png', '/android-chrome-512x512.png'],
+      manifest: {
+        "name": "Cartes.io",
+        "short_name": "Cartes",
+        "icons": [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone"
       }
     })
   ],
