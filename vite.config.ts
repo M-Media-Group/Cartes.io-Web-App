6da6bd289838282127e6efa27978158eaf70d834
Michal <mwargan@gmail.com>
2022-09-10 14:41:41 +0200

    Added theme color

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
             "type": "image/png"
           }
         ],
-        "theme_color": "#ffffff",
+        "theme_color": "#1095c1",
         "background_color": "#ffffff",
         "display": "standalone",
         "start_url": "/",
