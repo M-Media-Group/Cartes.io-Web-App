2bd4f04e7600a251e749095d0087ff3f249917b3
Michal <mwargan@gmail.com>
2022-09-10 14:09:36 +0200

    Added service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
           // treat all tags with a dash as custom elements
           isCustomElement: (tag) => tag.includes('a-')
         }
+      }
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
       }
     })
   ],
