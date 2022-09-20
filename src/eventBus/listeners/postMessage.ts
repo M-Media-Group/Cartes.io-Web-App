e5d2310a21937fe0b38d89067bdd9104888f17f9
Michal <mwargan@gmail.com>
2022-09-20 15:22:03 +0200

    Switched to parent context on postMessage events

--- src/eventBus/listeners/postMessage.ts
+++ src/eventBus/listeners/postMessage.ts
@@ -6,35 +6,35 @@ export default {
 
   created_map: (e: Map) => {
     // Send a postMessage event
-    window.postMessage({
+    window.parent.postMessage({
       type: eventTypes.created_map,
       data: e,
     }, '*');
   },
   updated_map: (e: Map) => {
     // Send a postMessage event
-    window.postMessage({
+    window.parent.postMessage({
       type: eventTypes.updated_map,
       data: e,
     }, '*');
   },
   deleted_map: (e: Map) => {
     // Send a postMessage event
-    window.postMessage({
+    window.parent.postMessage({
       type: eventTypes.deleted_map,
       data: e,
     }, '*');
   },
   created_marker: (e: Marker) => {
     // Send a postMessage event
-    window.postMessage({
+    window.parent.postMessage({
       type: eventTypes.created_marker,
       data: e,
     }, '*');
   },
   deleted_marker: (e: Marker) => {
     // Send a postMessage event
-    window.postMessage({
+    window.parent.postMessage({
       type: eventTypes.deleted_marker,
       data: e,
     }, '*');
