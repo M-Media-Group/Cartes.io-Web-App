0222ca789a504d147c2b127fd0ec65ceea126e17
Michal <mwargan@gmail.com>
2022-12-01 12:56:18 +0100

    Added module declaration

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -7,3 +7,5 @@ declare module '@vue-leaflet/vue-leaflet/src/utils';
 declare module 'leaflet/dist/leaflet-src.esm';
 
 declare module 'leaflet.markercluster/dist/leaflet.markercluster-src.js';
+
+declare module 'vue3-markdown-it';
